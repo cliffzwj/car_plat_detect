244e07926fc48228905100df7d9d8b538f47a07e
cliffzwj <cliffzwj@qq.com>
2018-01-19 11:45:28 +0800

    format code

--- detect_test.py
+++ detect_test.py
@@ -13,33 +13,35 @@ debugInfo = hyperConfig.configuration["global"]["debug"]
 testPath = hyperConfig.configuration["detectTest"]["detectPath"]
 outPath = hyperConfig.configuration["detectTest"]["outputPath"]
 
-def drawBoundingBox(originImage,rect):
-    cv2.rectangle(originImage, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])), (0, 0, 255), 2,
+
+def drawBoundingBox(originImage, rect):
+    cv2.rectangle(originImage, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])),
+                  (0, 0, 255), 2,
                   cv2.LINE_AA)
     return originImage
 
 
-#detect Plate in image batch
+# detect Plate in image batch
 def detectPlateBatchTest(filepath):
     for filename in os.listdir(filepath):
         if filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(".png"):
-            fileFullPath = os.path.join(filepath,filename)
+            fileFullPath = os.path.join(filepath, filename)
             image = cv2.imread(fileFullPath)
             image_c = image.copy()
             Plates = hyperDetect.detectPlateRough(image_c, image_c.shape[0], top_bottom_padding_rate=0.1)
             pathName = filename.split('.')[0]
             if debugInfo:
                 if len(Plates) != 0:
-                    if os.path.exists(os.path.join(outPath,pathName)) == False:
-                        os.mkdir(os.path.join(outPath,pathName))
-            for i,plate in enumerate(Plates):
+                    if os.path.exists(os.path.join(outPath, pathName)) == False:
+                        os.mkdir(os.path.join(outPath, pathName))
+            for i, plate in enumerate(Plates):
                 rect = plate[1]
                 region = plate[2]
                 if debugInfo:
-                    cv2.imwrite(os.path.join(outPath,pathName,"region_"+str(i)+"_"+pathName+".png"),region)
-                    drawBoundingBox(image_c,rect)
+                    cv2.imwrite(os.path.join(outPath, pathName, "region_" + str(i) + "_" + pathName + ".png"), region)
+                    drawBoundingBox(image_c, rect)
             if debugInfo:
-                cv2.imwrite(os.path.join(outPath,pathName,"out_"+pathName+".png"),image_c)
+                cv2.imwrite(os.path.join(outPath, pathName, "out_" + pathName + ".png"), image_c)
 
 
 # def main(args):
